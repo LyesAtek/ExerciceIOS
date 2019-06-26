db783be73337b49cf33b5e8c99efea4c3faaa2c4
Lyes ATEK <lyes.atek@gmail.com>
2019-06-26 16:41:22 +0200

    Preparation WebServices Call

---
+++ ExerciceIOS/ExerciceIOS/ExerciceIOS/Model/GuildWars2API.swift
@@ -0,0 +1,14 @@
+//
+//  GuildWars2API.swift
+//  ExerciceIOS
+//
+//  Created by Lyes ATEK on 26/06/2019.
+//  Copyright © 2019 Lyes ATEK. All rights reserved.
+//
+
+import Foundation
+
+class GuildWars2API{
+    
+     static let baseUrl = "https://api.guildwars2.com/v2/"
+}
